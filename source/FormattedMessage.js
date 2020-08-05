a7f84883f2e67210b17bbe6d6f524f18b964267c
Emanuel Rabina <emanuelrabina@gmail.com>
2020-08-06 11:03:16 +1200

    Create <FormattedMessage> for formatting strings in React

---
+++ source/FormattedMessage.js
@@ -0,0 +1,29 @@
+
+import withMessageFormatter from './withMessageFormatter.js';
+
+import PropTypes          from 'prop-types';
+import React, {Component} from 'react';
+
+/**
+ * React wrapper for the ICU message formatter's `format` method, using the
+ * props and context to pass along to that method.
+ */
+export default withMessageFormatter(class FormattedMessage extends Component {
+
+	static propTypes = {
+		formatter: PropTypes.object,
+		id: PropTypes.string.isRequired,
+		locale: PropTypes.string,
+		messages: PropTypes.object,
+		values: PropTypes.object
+	};
+
+	/**
+	 * @return {*}
+	 */
+	render() {
+
+		let {formatter, id, locale, messages, values} = this.props;
+		return formatter.format(messages[id], values, locale);
+	}
+});
--- source/IcuMessageFormatter.js
+++ source/IcuMessageFormatter.js
@@ -1,3 +1,4 @@
 
 export {MessageFormatterProvider as default} from './MessageFormatterProvider.js';
 export {withMessageFormatter as default}     from './withMessageFormatter.js';
+export {FormattedMessage as default}         from './FormattedMessage.js';
