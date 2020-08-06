a4ee58247bf3c17c8c1e5bb9a0bead65a730500b
Emanuel Rabina <emanuelrabina@gmail.com>
2020-08-07 10:55:59 +1200

    Build with rollup

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+
+import babel       from '@rollup/plugin-babel';
+import commonjs    from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+
+export default {
+	input: 'source/IcuMessageFormatter.js',
+	output: [
+		{
+			file: `lib/react-icu-message-formatter.cjs.js`,
+			format: 'cjs',
+			sourcemap: true
+		},
+		{
+			file: `lib/react-icu-message-formatter.es.js`,
+			format: 'es',
+			sourcemap: true
+		}
+	],
+	plugins: [
+		babel({
+			babelHelpers: 'runtime'
+		}),
+		commonjs(),
+		nodeResolve()
+	],
+	external: [
+		/@babel\/runtime/,
+		'@ultraq/icu-message-formatter',
+		'prop-types',
+		'react'
+	]
+};
