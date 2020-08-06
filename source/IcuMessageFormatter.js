f1b109fc288a2047c7cee0436fe832be7a4bffca
Emanuel Rabina <emanuelrabina@gmail.com>
2020-08-06 15:35:59 +1200

    Forgot to export FormattedHtmlMessage

--- source/IcuMessageFormatter.js
+++ source/IcuMessageFormatter.js
@@ -2,3 +2,4 @@
 export {default as MessageFormatterProvider} from './MessageFormatterProvider.js';
 export {default as withMessageFormatter}     from './withMessageFormatter.js';
 export {default as FormattedMessage}         from './FormattedMessage.js';
+export {default as FormattedHtmlMessage}     from './FormattedHtmlMessage.js';
