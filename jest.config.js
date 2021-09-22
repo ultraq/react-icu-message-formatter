01e7980bc1388fa6e717adb6f1ef49920115a580
Emanuel Rabina <emanuelrabina@gmail.com>
2021-09-22 20:13:52 +1200

    Another round of package updates

--- jest.config.js
+++ jest.config.js
@@ -21,5 +21,6 @@ module.exports = {
 	},
 	setupFiles: [
 		'./jest.setup.js'
-	]
+	],
+	testEnvironment: 'jsdom'
 };
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 
-import babel       from '@rollup/plugin-babel';
-import commonjs    from '@rollup/plugin-commonjs';
-import nodeResolve from '@rollup/plugin-node-resolve';
+import {babel}       from '@rollup/plugin-babel';
+import commonjs      from '@rollup/plugin-commonjs';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
 
 export default {
 	input: 'source/IcuMessageFormatter.js',
